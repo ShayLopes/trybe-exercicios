21cdac66ba9a632df914b3813cf4e7009b9927ab
ShayLopes <shaytom71@gmail.com>
2022-10-26 21:06:28 -0300

    Exercicios resolvidos

--- 01-fundamentals/secao-04-introducao-a-javascript/dia-02-array-loop-for/exercicios.js
+++ 01-fundamentals/secao-04-introducao-a-javascript/dia-02-array-loop-for/exercicios.js
@@ -36,3 +36,61 @@ for (i of names){
 
 //--------------------------------------//
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+let maiorValor = 0;
+let qntImpar = 0;
+let menorValor = 0;
+
+for(i of numbers){
+    soma += i;
+    console.log(i);
+}
+
+media = soma/numbers.length;
+
+console.log (soma);
+console.log (media);
+
+if(media > 20){
+    console.log('valor maior que 20');
+} else {
+    console.log('valor menor que 20');
+}
+
+for(j of numbers){
+    if(maiorValor < j){
+        maiorValor = j;
+    }
+    if(menorValor+numbers[1] > j){
+        menorValor = j;
+    }
+}
+
+console.log(maiorValor);
+console.log(menorValor);
+
+for(m of numbers){
+    if(m % 2 !== 0){
+        qntImpar++;
+    }
+}
+
+console.log(qntImpar);
+
+if(qntImpar === 0){
+    console.log('nenhum valor ímpar encontrado');
+}
+
+let array = [ ];
+
+for(n = 0; n <= 25; n += 1){
+    array.push(n);
+    console.log(array[n]);
+    console.log(array[n]/2);
+}
+
+// fim // 
+
+
